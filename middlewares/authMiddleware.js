9a69f704254a0b8b3b3162d1e0d1afb4d38722c0
agustinmota <agustinmotatr@gmail.com>
2025-09-15 17:36:56 -0300

    auth middleware
    protect the routes that need to be logged in to have access

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,10 @@
+const {expressjwt: jwt} = require("express-jwt");
+
+
+
+const authMiddleware = jwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"]
+});
+
+module.exports = authMiddleware;
--- routes/accommodationRoutes.js
+++ routes/accommodationRoutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const AccommodationControllers = require('../controllers/AccommodationControllers');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
 
 
-router.post('/edit/:id',AccommodationControllers.edit);
-router.delete('/delete/:id',AccommodationControllers.destroy);
-router.post('/create',AccommodationControllers.create);
 router.get('/index',AccommodationControllers.index);
 router.get('/show/:id', AccommodationControllers.show);
+router.use(authMiddleware);
+router.post('/create',AccommodationControllers.create);
+router.post('/edit/:id',AccommodationControllers.edit);
+router.delete('/delete/:id',AccommodationControllers.destroy);
 
 
 module.exports=router;
