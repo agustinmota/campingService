c00c4a3c0dfa0b30a26913f8aac38ed2fbc898ad
agustinmota <agustinmotatr@gmail.com>
2026-09-23 11:22:18 -0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -12,8 +12,14 @@ const hashPassword = require ("../utils/hashPassword");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
-  logging: console.log
-
+  port: process.env.DB_PORT,
+  logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 async function init() {
